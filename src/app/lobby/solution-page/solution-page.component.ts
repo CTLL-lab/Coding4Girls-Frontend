dde1f6cb809a5a618b4f991008d260ba56b1e68c
Spanagiot <spanagiot95@gmail.com>
2019-08-19 17:07:13 +0300

    implement solution page, only for challenge so far

--- src/app/lobby/solution-page/solution-page.component.ts
+++ src/app/lobby/solution-page/solution-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Subscription } from 'rxjs';
+import { ChallengeService } from '../services/challenge/challenge.service';
 
 @Component({
   selector: 'app-solution-page',
@@ -6,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./solution-page.component.css']
 })
 export class SolutionPageComponent implements OnInit {
+  public worldBehaviorSubject: BehaviorSubject<any> = new BehaviorSubject(null);
+  public worldSubscription: Subscription;
 
-  constructor() { }
+  private userID = this.route.snapshot.paramMap.get('userID');
+  private challengeID = this.route.snapshot.paramMap.get('challengeID');
 
-  ngOnInit() {
+  constructor(
+    private route: ActivatedRoute,
+    private challengeService: ChallengeService
+  ) {
+    window['world'] = this.worldBehaviorSubject;
   }
 
+  ngOnInit() {
+    this.worldSubscription = this.worldBehaviorSubject.subscribe(x => {
+      if (x == null) {
+        return;
+      }
+      this.challengeService
+        .GetChallengeSnapSolutionFromUser(this.challengeID, this.userID)
+        .subscribe(y => {
+          x.children[0].openProjectString(
+            y['data']['solution']['snap_solution']
+          );
+        });
+      this.worldSubscription.unsubscribe();
+    });
+  }
 }
