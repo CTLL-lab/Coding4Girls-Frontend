8116cf5900b271f574387d63c7d0cc880930bee1
Spanagiot <spanagiot95@gmail.com>
2019-08-30 01:58:58 +0300

    make notification titles translatable

--- src/app/shared/services/toaster/notifications-toaster.service.ts
+++ src/app/shared/services/toaster/notifications-toaster.service.ts
@@ -1,30 +1,37 @@
 import { Injectable } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationsToasterService {
-  constructor(private toaster: ToastrService) {}
+  constructor(
+    private toaster: ToastrService,
+    private translation: TranslateService
+  ) {}
   showSuccess(message: string) {
-    this.toaster.success(message, 'Success', {
-      positionClass: 'toast-bottom-right',
-      progressBar: true,
-      timeOut: 3500
-    });
+    this.translation
+      .get('in-code.13')
+      .toPromise()
+      .then(x => {
+        this.toaster.success(message, x, {
+          positionClass: 'toast-bottom-right',
+          progressBar: true,
+          timeOut: 3500
+        });
+      });
   }
   showError(message: string) {
-    this.toaster.error(message, 'Error', {
-      positionClass: 'toast-bottom-right',
-      progressBar: true,
-      timeOut: 3500
-    });
-  }
-  showAchievement(message: string) {
-    this.toaster.info(message, 'Achievement unlocked', {
-      positionClass: 'toast-bottom-right',
-      progressBar: true,
-      timeOut: 3500
-    });
+    this.translation
+      .get('in-code.14')
+      .toPromise()
+      .then(x => {
+        this.toaster.error(message, x, {
+          positionClass: 'toast-bottom-right',
+          progressBar: true,
+          timeOut: 3500
+        });
+      });
   }
 }
