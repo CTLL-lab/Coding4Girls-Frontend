3b93931bead2c98956fe683ef680a49d8868fb9b
Spanagiot <spanagiot95@gmail.com>
2019-08-30 01:19:41 +0300

    update languages and flags for the project

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,9 +1,42 @@
-export const apiURL = 'http://10.2.1.5:5000';
+export const apiURL = 'http://localhost:5000';
 // export const apiURL = 'https://api.codinggirls.spanagiot.gr';
 export const imgurAPI = 'https://api.imgur.com/3';
 export const priviledged_roles = ['admin', 'teacher', 'sysadmin'];
 export const fully_priviledged_roles = ['admin', 'sysadmin'];
-export const languages_available = ['en'];
-export const languages_flags_assets = [
-  'assets/FlagIcons/flag_united_kingdom.png'
+export const languages_available = [
+  {
+    name: 'English',
+    imagePath: 'assets/FlagIcons/flag_united_kingdom.png',
+    code: 'en'
+  },
+  {
+    name: 'Greek',
+    imagePath: 'assets/FlagIcons/flag_greece.png',
+    code: 'el'
+  },
+  {
+    name: 'Turkish',
+    imagePath: 'assets/FlagIcons/flag_turkey.png',
+    code: 'tr'
+  },
+  {
+    name: 'Bulgarian',
+    imagePath: 'assets/FlagIcons/flag_bulgaria.png',
+    code: 'bg'
+  },
+  {
+    name: 'Croatian',
+    imagePath: 'assets/FlagIcons/flag_croatia.png',
+    code: 'hr'
+  },
+  {
+    name: 'Slovenian',
+    imagePath: 'assets/FlagIcons/flag_slovenia.png',
+    code: 'sl'
+  },
+  {
+    name: 'Portugese',
+    imagePath: 'assets/FlagIcons/flag_portugal.png',
+    code: 'pt'
+  }
 ];
--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
-import { languages_available, languages_flags_assets } from 'src/app/config';
+import { languages_available } from 'src/app/config';
 import { AuthenticationService } from 'src/app/authentication/services/auth/authentication.service';
 import { UserService } from 'src/app/authentication/services/user/user.service';
 import { BehaviorSubject } from 'rxjs';
@@ -14,28 +14,24 @@ import { User } from 'src/app/authentication/services/user/user';
 })
 export class NavbarComponent implements OnInit {
   user: User;
+  counter = 0;
+  public currentLanguage;
+  public languages_available = languages_available;
   constructor(
     public auth: AuthenticationService,
     private router: Router,
     public userService: UserService,
     public translate: TranslateService
   ) {}
-  counter = 0;
-  language = 'en';
-  imagePath: string;
-  languages_available;
-  languages_flags_assets;
 
   ngOnInit() {
     this.userService.user.subscribe(x => {
       this.user = x;
     });
-    this.imagePath = 'assets/FlagIcons/flag_united_kingdom.png';
+    this.currentLanguage = languages_available[0];
     // this.user.getUserLanguage().subscribe(r => {
     //   this.changeFlagToLanguage(r);
     // });
-    this.languages_available = languages_available;
-    this.languages_flags_assets = languages_flags_assets;
   }
 
   logout() {
@@ -44,19 +40,12 @@ export class NavbarComponent implements OnInit {
   }
 
   public toggleNextFlag() {
-    //   this.counter = (this.counter + 1) % languages_available.length;
-    //   this.imagePath = languages_flags_assets[this.counter];
-    //   this.changeLanguageTo(languages_available[this.counter]);
+    this.counter = (this.counter + 1) % this.languages_available.length;
+    this.currentLanguage = this.languages_available[this.counter];
+    this.changeLanguageTo(this.currentLanguage.code);
   }
   public changeLanguageTo(lang: string) {
     //   this.changeFlagToLanguage(lang);
     //   this.user.changeUserLanguage(lang).subscribe();
-  }
-  public changeFlagToLanguage(lang: string) {
-    this.counter = languages_available.findIndex(x => x === lang);
-    if (this.counter < 0) {
-      this.counter = 0;
-    }
-    this.imagePath = languages_flags_assets[this.counter];
   }
 }
