c9834973364ae009b102efa5e78c06c76a380adb
Spanagiot <spanagiot95@gmail.com>
2019-08-29 20:31:27 +0300

    solution page now shows also lobby pages

--- src/app/lobby/solution-page/solution-page.component.ts
+++ src/app/lobby/solution-page/solution-page.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Subscription } from 'rxjs';
 import { ChallengeService } from '../services/challenge/challenge.service';
+import { AnswersService } from '../services/answers/answers.service';
 
 @Component({
   selector: 'app-solution-page',
@@ -15,10 +16,11 @@ export class SolutionPageComponent implements OnInit {
 
   private userID = this.route.snapshot.paramMap.get('userID');
   private challengeID = this.route.snapshot.paramMap.get('challengeID');
-
+  private lobbyID = this.route.snapshot.paramMap.get('id');
   constructor(
     private route: ActivatedRoute,
-    private challengeService: ChallengeService
+    private challengeService: ChallengeService,
+    private answersService: AnswersService
   ) {
     window['world'] = this.worldBehaviorSubject;
   }
@@ -28,14 +30,24 @@ export class SolutionPageComponent implements OnInit {
       if (x == null) {
         return;
       }
-      this.challengeService
-        .GetChallengeSnapSolutionFromUser(this.challengeID, this.userID)
-        .subscribe(y => {
-          x.children[0].openProjectString(
-            y['data']['solution']['snap_solution']
-          );
-        });
-      this.worldSubscription.unsubscribe();
+      if (this.route.snapshot.data['lobby']) {
+        this.answersService
+          .GetLobbySolutionForUser(this.userID, this.lobbyID)
+          .subscribe(y => {
+            x.children[0].openProjectString(
+              y['data']['solution']['snap_solution']
+            );
+          });
+      } else {
+        this.challengeService
+          .GetChallengeSnapSolutionFromUser(this.challengeID, this.userID)
+          .subscribe(y => {
+            x.children[0].openProjectString(
+              y['data']['solution']['snap_solution']
+            );
+          });
+        this.worldSubscription.unsubscribe();
+      }
     });
   }
 }
