f9cf5554d77a47479fd6bb71c8692f14eea9f100
Spanagiot <spanagiot95@gmail.com>
2019-09-10 14:57:22 +0300

    prefill variables in minigames with simple variables

--- src/app/lobby/minigames/form-element/form-element.component.ts
+++ src/app/lobby/minigames/form-element/form-element.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, NgControlStatus } from '@angular/forms';
 
 @Component({
   selector: 'app-form-element',
--- src/app/lobby/minigames/simple/simple.component.ts
+++ src/app/lobby/minigames/simple/simple.component.ts
@@ -11,7 +11,27 @@ import { MinigameService } from '../minigame.service';
 export class SimpleComponent implements OnInit {
   @Input() variables;
   @Input() form: FormGroup;
+  @Input() prefilled_variables;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    // if prefilled variables is {} and doesn't contain questions array
+    try {
+      this.prefilled_variables;
+    } catch {
+      // make it undefined
+      this.prefilled_variables = undefined;
+    }
+
+    if (this.prefilled_variables != undefined) {
+      for (let key in this.prefilled_variables) {
+        try {
+          this.form.get(key).setValue(this.prefilled_variables[key]);
+        } catch (err) {
+          continue;
+        }
+      }
+    }
+  }
 }
