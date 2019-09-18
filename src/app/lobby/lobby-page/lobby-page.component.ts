94129abfc13573bc668bf87aafc43a06039b9aac
Spanagiot <spanagiot95@gmail.com>
2019-09-18 23:58:30 +0300

    clean up lobby page

--- src/app/lobby/lobby-page/lobby-page.component.ts
+++ src/app/lobby/lobby-page/lobby-page.component.ts
@@ -1,16 +1,8 @@
-import {
-  Component,
-  OnInit,
-  ViewChild,
-  AfterContentChecked,
-  TemplateRef
-} from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from 'src/app/authentication/services/user/user.service';
 import { LobbyService } from '../../shared/services/lobby/lobby.service';
-import { ChallengeService } from '../services/challenge/challenge.service';
 import { ChallengeBoxOptions } from '../challenge-box/challenge-box.component';
-import { priviledged_roles } from '../../config';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { MinigameService } from '../minigames/minigame.service';
 import { NotificationsToasterService } from 'src/app/shared/services/toaster/notifications-toaster.service';
@@ -21,15 +13,12 @@ import { NotificationsToasterService } from 'src/app/shared/services/toaster/not
   styleUrls: ['./lobby-page.component.css'],
   providers: []
 })
-export class LobbyPageComponent implements OnInit, AfterContentChecked {
-  @ViewChild('settingsModel')
-  settingsModel;
+export class LobbyPageComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     public userService: UserService,
     private lobbyService: LobbyService,
     private router: Router,
-    private challengeService: ChallengeService,
     private modalService: BsModalService,
     public minigamesService: MinigameService,
     public notifications: NotificationsToasterService
@@ -47,26 +36,15 @@ export class LobbyPageComponent implements OnInit, AfterContentChecked {
   public SortableJSOptions;
   public id: string;
   public lobbyDetails: any;
-  public newName: string;
-  public showCompleted = false;
-  public teamForModal;
   public challenges = [];
-  items = [1, 2, 3, 4, 5];
   public members: Array<any>;
 
   public currentDraggingBox = -1;
-
-  ngAfterContentChecked() {}
 
   ngOnInit() {
     this.id = this.route.snapshot.paramMap.get('id');
     this.lobbyService.getLobbyDetails(this.id).subscribe(r => {
       this.lobbyDetails = r['data']['lobby'];
-      //   this.userService
-      //     .getUsernameFromID(this.lobbyDetails.createdby)
-      //     .subscribe(creator => {
-      //       this.lobbyDetails.creatorusername = creator;
-      //     });
     });
     this.lobbyService.GetLobbyChallenges(this.id).subscribe(r => {
       // first we store temporarily the challenges
@@ -74,76 +52,23 @@ export class LobbyPageComponent implements OnInit, AfterContentChecked {
       // and their order
       const challengesOrder = r['data']['order'];
       // and we recreate the challenges list based on this order
-      for (let i of challengesOrder) {
-        this.challenges.push(challengesTemp.find(x => x.id == i));
+      if (challengesOrder != null) {
+        for (let i of challengesOrder) {
+          this.challenges.push(challengesTemp.find(x => x.id == i));
+        }
       }
-      // this.challenges = r['data']['challenges'];
     });
     this.lobbyService.getLobbyMembers(this.id).subscribe(r => {
-      console.log(r);
       this.members = r['data']['members'];
     });
   }
-
-  lockLobby() {
-    this.lobbyService.lockLobby(this.id).subscribe(r => {
-      if (r['success']) {
-        this.lobbyDetails.locked = true;
-      }
-    });
-  }
-
-  unlockLobby() {
-    this.lobbyService.unlockLobby(this.id).subscribe(r => {
-      if (r['success']) {
-        this.lobbyDetails.locked = false;
-      }
-    });
-  }
-
-  showHideCompleted() {
-    this.showCompleted = !this.showCompleted;
-  }
-
-  teamJoin(team: any) {
-    // this.dataPasser.reviewMode = false;
-    this.challengeService.joinTeam(team.id).subscribe(() => {
-      // const username = this.userService.getUsername();
-      // this.notesProvider.memberJoin(this.id, team.id, username);
-      // team.members.push(username);
-    });
-  }
-
-  teamLeave(team: any) {}
 
   openChallengeSettings(id: string) {
     this.router.navigateByUrl('/lobby/team/' + id + '/edit');
   }
 
   openSettings() {
-    // this.settingsModel.nativeElement.style.display = 'initial';
     this.router.navigateByUrl('/lobby/' + this.id + '/settings');
-  }
-  openModal(team: any, modalToShow: TemplateRef<any>) {
-    this.teamForModal = team;
-    this.modalService.show(modalToShow);
-  }
-
-  closeAllModals() {
-    for (let i = 1; i <= this.modalService.getModalsCount(); i++) {
-      this.modalService.hide(i);
-    }
-    document.body.classList.remove('modal-open');
-  }
-
-  changeName(event: { team: any; newName: string }) {
-    this.challengeService
-      .changeTeamName(event.team.id, event.newName)
-      .subscribe(r => {
-        if (r['success']) {
-          event.team.name = event.newName;
-        }
-      });
   }
 
   canCreateTeam() {
@@ -182,5 +107,12 @@ export class LobbyPageComponent implements OnInit, AfterContentChecked {
         this.notifications.showSuccess('');
         this.router.navigateByUrl('/lobbies');
       });
+  }
+
+  closeAllModals() {
+    for (let i = 1; i <= this.modalService.getModalsCount(); i++) {
+      this.modalService.hide(i);
+    }
+    document.body.classList.remove('modal-open');
   }
 }
