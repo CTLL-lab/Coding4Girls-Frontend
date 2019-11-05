30c7b0bd31dc6bc45c16a90b86125ea4b6ab8283
Spanagiot <spanagiot95@gmail.com>
2019-11-05 14:30:22 +0200

    make token providable in url parameters, solution for unity

--- src/app/authentication/services/auth-guard/auth-guard.service.ts
+++ src/app/authentication/services/auth-guard/auth-guard.service.ts
@@ -2,21 +2,36 @@ import { Injectable } from '@angular/core';
 import {
   CanActivate,
   Router,
-  RouterStateSnapshot,
-  ActivatedRouteSnapshot
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot
 } from '@angular/router';
 import { UserService } from '../user/user.service';
+import { AuthenticationService } from '../auth/authentication.service';
 
 @Injectable()
 export class AuthGuardService implements CanActivate {
-  constructor(private router: Router, private user: UserService) {}
+  constructor(
+    private user: UserService,
+    private router: Router,
+    private auth: AuthenticationService
+  ) {}
 
-  canActivate(): boolean {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean {
     if (this.user.GetUserID()) {
       return true;
     } else {
-      this.router.navigate(['/login']);
-      return false;
+      try {
+        this.auth.DecodeToken(route.queryParams['token']);
+        this.auth.storeUser(route.queryParams['token'], false);
+        return true;
+      } catch (err) {
+        console.error(err);
+        this.router.navigate(['/login']);
+        return false;
+      }
     }
   }
 }
